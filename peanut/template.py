c5c4c8269a4a17a89cad83f7a94fa7756f411015
zqqf16 <zqqf16@gmail.com>
2015-10-30 18:15:28 +0800

    Init template

---
+++ peanut/template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Template"""
+
+import jinja2
+
+from jinja2 import ChoiceLoader, FileSystemLoader, PackageLoader
+
+
+loader = jinja2.ChoiceLoader([
+    FileSystemLoader('/path/to/local/template'),
+    PackageLoader('peanut', 'templates/default')
+])
