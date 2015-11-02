60f0cf3e779e31989f2e7095d41f3e7305c281b2
zqqf16 <zqqf16@gmail.com>
2015-11-02 17:24:16 +0800

    Init template

--- peanut/template.py
+++ peanut/template.py
@@ -3,12 +3,48 @@
 
 """Template"""
 
+import peanut
 import jinja2
+from os import path
+from jinja2 import FileSystemLoader
+from jinja2.exceptions import TemplateNotFound
 
-from jinja2 import ChoiceLoader, FileSystemLoader, PackageLoader
+
+class SmartLoader(FileSystemLoader):
+    """A smart template loader"""
+
+    available_extension = ['.html', '.xml']
+
+    def get_source(self, environment, template):
+        if template is None:
+            raise TemplateNotFound(template)
+        if '.' in template:
+            return super(SmartLoader, self).get_source(environment, template)
+
+        for extension in SmartLoader.available_extension:
+            try:
+                filename = template + extension
+                return super(SmartLoader, self).get_source(environment, filename)
+            except TemplateNotFound:
+                pass
+
+        raise TemplateNotFound(template)
 
 
-loader = jinja2.ChoiceLoader([
-    FileSystemLoader('/path/to/local/template'),
-    PackageLoader('peanut', 'themes/default')
-])
+class Template(object):
+    """Template"""
+
+    def __init__(self, path, **kwargs):
+        loader = SmartLoader(path)
+        self.env = jinja2.Environment(
+            loader=loader,
+            autoescape=True
+        )
+        # Update global namesapce
+        self.env.globals.update(kwargs)
+
+    def render(self, name, **context):
+        """Render template with name and context"""
+
+        template = self.env.get_template(name)
+        template.render(**context)
