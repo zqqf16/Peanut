1eba84dff33b15a8e6487c5de17f64d67bc28d58
zqqf16 <zqqf16@gmail.com>
2015-11-03 17:02:18 +0800

    Add `abs_url` filter

--- peanut/context.py
+++ peanut/context.py
@@ -3,6 +3,11 @@
 
 """Context for template rendering"""
 
+try:
+    from urllib.parse import urljoin
+except:
+    from urlparse import urljoin
+
 from datetime import datetime
 from peanut.config import config
 
@@ -14,15 +19,18 @@ __all__ = ['filters']
 def asset(value):
     """Get asset file url"""
     asset_path = config.path['asset']
-    if not asset_path.endswith('/'):
-        asset_path += '/'
-    return asset_path+value
+    return urljoin(asset_path, value)
 
 def strftime(value, date_format):
     """Date formatter"""
     return datetime.strftime(value, date_format)
 
+def abs_url(value):
+    """Absolute url"""
+    return urljoin(config.site['url'], value)
+
 filters = {
     'asset': asset,
     'strftime': strftime,
+    'abs_url': abs_url,
 }
