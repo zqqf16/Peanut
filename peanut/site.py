4786098c090697021df005bd7e3d965e89324506
zqqf16 <zqqf16@gmail.com>
2015-11-05 17:18:17 +0800

    Encapsulate site operations to Site class

--- peanut/site.py
+++ peanut/site.py
@@ -4,110 +4,178 @@
 import os
 import codecs
 
+import peanut
 import peanut.reader as reader
-from peanut.config import config
 from peanut.model import SinglePage, Tag, Category, Post
+from peanut.config import default_config
 from peanut.template import Template
-from peanut.context import filters
-from peanut.utils import neighborhood, url2pathname
-
-def render_post(template, post):
-    template.render(post.layout, post=post)
-    pass
-
-def render_tag(template, tag):
-    pass
-
-def render_category(template, category):
-    pass
-
-def render_static_files(template, static_file):
-    pass
-
-def generate():
-    """Generate all site files"""
-
-    context = {
-        'site': config.site,
-        'author': config.author,
-    }
-
-    template = Template(config.theme_path, filters=filters, **context)
-
-    # posts
-    for prev, post, next in neighborhood(Post.all()):
-        content = template.render(post.layout, post=post,
-                prev_post=prev, next_post=next)
-        write_html(post.file_path, content)
-
-    # tags
-    for tag in Tag.all():
-        content = template.render(Tag.layout, tag=tag, posts=tag.posts)
-        write_html(tag.file_path, content)
-
-    for page in ('index', 'rss', 'sitemap'):
-        single = SinglePage(page, page)
-        single.layout = page
-
-        content = template.render(single.layout, posts=Post.all(),
-                tags=Tag.all())
-        write_html(single.file_path, content)
+from peanut.context import get_filters
 
 
-def write_html(path, content):
-    """Write HTML file"""
+class Site(object):
+    """Site"""
 
-    path = os.path.join(config.curdir, path)
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    with codecs.open(path, 'w') as f:
-        f.write(content)
+    default_config = ['_config.yaml', 'config.yaml',
+            '_config.yml', 'config.yml']
 
+    def __init__(self, directory='.', config_path=None):
+        self.curdir = directory
+        self.config = self.load_config(config_path)
 
-def create_post(draft):
-    """Create post with draft"""
+        self.template = Template(
+                self.config.theme_path,
+                filters=get_filters(self.config),
+                site=self.config.site,
+                author=self.config.author)
 
-    if not draft.get('title') or not draft.get('slug'):
-        return
+    @property
+    def posts(self):
+        return Post.all()
 
-    Post.create(**draft)
+    @property
+    def tags(self):
+        return Tag.all()
 
-    if draft.get('tags'):
-        for tag in draft['tags']:
-            Tag.create(tag)
-    if draft.get('catetory'):
-        for category in draft['category']:
-            Category.create(category)
+    @property
+    def categories(self):
+        return Category.all()
 
+    @property
+    def single_pages(self):
+        return SinglePage.all()
 
-def read_all_drafts(path):
-    """Read all drafts from path"""
+    @property
+    def get_file_path(self, item):
+        template = self.config.path[item.layout]
+        if not template:
+            raise KeyError('Path for {} not found'.format(item.layout))
 
-    for f in os.listdir(path):
-        if f.startswith('.'):
-            continue
+        return template.format(**item.__dict__)
 
-        p = os.path.join(path, f)
-        if os.path.isdir(p):
-            read_all_drafts(p)
-            continue
+    def load_config(self, config_path):
+        """Load config file from file
+        """
+        config = default_config()
+        config.curdir = self.curdir
 
-        draft = reader.read(p)
-        if draft:
-            create_post(draft)
+        if not config_path:
+            for file_name in self.default_config:
+                p = os.path.join(self.curdir, file_name)
+                if os.path.isfile(p):
+                    config_path = p
+                    break
+            else:
+                raise FileNotFoundError('Config file not found at directory \
+{}'.format(self.curdir))
+        else:
+            if not os.path.isfile(config_path):
+                raise FileNotFoundError('Config file not found at path {}'\
+                        .format(config_path))
 
-
-
-def start(directory='.', config_path=None):
-    """Start loading drafts and generationg HTML files"""
-
-    config.curdir = directory
-    if config_path:
-        if not os.path.isfile(config_path):
-            raise FileNotFoundError('Config file not found at path {}'\
-                    .format(config_path))
         config.load(config_path)
 
-    draft_path = os.path.join(directory, config.path.draft)
-    read_all_drafts(draft_path)
+        Post.path_template = config.path.post
+        Tag.path_template = config.path.tag
+        Category.path_template = config.path.category
 
-    generate()
+        return config
+
+    def load_drafts(self):
+        """Load all drafts
+        """
+        draft_dir = os.path.join(self.curdir, self.config.path.draft)
+        for f in os.listdir(draft_dir):
+            if f.startswith('.'):
+                continue
+            p = os.path.join(draft_dir, f)
+            self.parse_draft(p)
+
+        # Create index, rss and sitemap
+        for page in ('index', 'rss', 'sitemap'):
+            single_page = SinglePage.create(page, page)
+            single_page.path_template = self.config.path[page]
+
+
+    def parse_draft(self, draft_file):
+        """Parse draft file
+        """
+        draft = reader.read(draft_file)
+        if not draft:
+            return
+
+        if not draft.get('title') or not draft.get('slug'):
+            return
+
+        if not draft.get('publish', True):
+            return
+
+        Post.create(**draft)
+
+        if draft.get('tags'):
+            for tag in draft['tags']:
+                Tag.create(tag)
+        if draft.get('catetory'):
+            for category in draft['category']:
+                Category.create(category)
+
+    def _get_post_naighbor(self, post):
+        """Get previous and next post
+        """
+        posts = self.posts
+        index = posts.index(post)
+        prev = None
+        next = None
+        if index > 0:
+            prev = posts[index-1]
+        if index < len(posts)-1:
+            next = posts[index+1]
+
+        return (prev, next)
+
+    def render_post(self, post):
+        """Render post
+        """
+        prev, next = self._get_post_naighbor(post)
+        return self.template.render(post.layout, post=post,
+                prev_post=prev, next_post=next)
+
+    def render_tag(self, tag):
+        """Render tag
+        """
+        return self.template.render(tag.layout, tag=tag, posts=tag.posts)
+
+    def render_category(self, category):
+        """Render category
+        """
+        return self.template.render(category.layout, category=category,
+                posts=category.posts)
+
+    def render_single_page(self, page):
+        """Render single page
+        """
+        return self.template.render(page.layout)
+
+    def write_html(self, item, content):
+        """Write HTML file"""
+
+        path = os.path.join(self.config.curdir, item.file_path)
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with codecs.open(path, 'w') as f:
+            f.write(content)
+
+    def generate(self):
+        """Generate static site
+        """
+        self.load_drafts()
+        for post in self.posts:
+            content = self.render_post(post)
+            self.write_html(post, content)
+        for tag in self.tags:
+            content = self.render_tag(tag)
+            self.write_html(tag, content)
+        for category in self.categories:
+            content = self.render_category(category)
+            self.write_html(category, content)
+        for page in self.single_pages:
+            content = self.render_single_page(page)
+            self.write_html(page, content)
