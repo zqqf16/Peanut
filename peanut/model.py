7ab9baa49f568a02b4c72dcc9228befc79a5cb23
zqqf16 <zqqf16@gmail.com>
2015-11-06 12:44:01 +0800

    Refactor model

--- peanut/model.py
+++ peanut/model.py
@@ -6,180 +6,47 @@
 
 from __future__ import unicode_literals
 
-import inspect
-
-from six import with_metaclass
 from datetime import datetime
 
-from peanut.utils import path_to_url
+from peanut.utils import path_to_url, url_safe
+from peanut.options import configs, env
 
 
-class ObjectPool(type):
-    """Meta class to implement a simple "object pool".
+class Post(object):
+    """Post model
     """
-
-    def __new__(mcs, name, bases, attrs):
-        """Add an attribute "_pool".
-        """
-        attrs['_pool'] = {}
-        return super(ObjectPool, mcs).__new__(mcs, name, bases, attrs)
-
-
-class BaseModel(with_metaclass(ObjectPool, object)):
-    """Base model"""
-
-    # Pool identity key
-    _identity = 'slug'
-
-    # layout
-    layout = 'page'
-
-    # file path template
-    path_template = u'{slug}.html'
-
-    @classmethod
-    def all(cls):
-        """Get all instance from pool"""
-        return cls._pool.values()
-
-    @classmethod
-    def get(cls, identity):
-        """Get an instance from pool by identity"""
-        return cls._pool.get(identity)
-
-    @classmethod
-    def create(cls, *args, **kwargs):
-        """Create an instance"""
-        instance = cls(*args, **kwargs)
-        cls._pool[instance.identity] = instance
-        return instance
-
-    @classmethod
-    def get_or_create(cls, *args, **kwargs):
-        """Get an instance from pool, if does not exist, create a new one"""
-        id_key = getattr(cls, '_identity', None)
-        call_args = inspect.getcallargs(cls.__init__, None, *args, **kwargs)
-        identity = call_args.get(id_key)
-        instance = cls.get(identity, None)
-        return instance or cls.create(*args, **kwargs)
-
-    @classmethod
-    def add(cls, instance):
-        cls._pool[instance.identity] = instance
-
-    @property
-    def identity(self):
-        return self.__dict__[self._identity]
-
-    def __init__(self, title, slug):
-        self.title = title
-        self.slug = slug.replace(' ', '_')
-
-    @property
-    def url(self):
-        path = self.file_path
-        if not path.startswith('/'):
-            path = '/' + path
-        return path_to_url(path)
-
-    @property
-    def file_path(self):
-        try:
-            return self.path_template.format(**self.__dict__)
-        except:
-            print(self.title)
-            print(self.__dict__)
-            exit()
-
-
-class SinglePage(BaseModel):
-    '''Single page, such as index, rss and sitemap'''
-    pass
-
-
-class Post(BaseModel):
-    '''Post'''
 
     layout = 'post'
 
-    def __init__(self, title, slug, content=None, date=None,
-                 publish=True, top=False, layout='post',
-                 tags=None, category=None, **kwargs):
-
-        super(Post, self).__init__(title=title, slug=slug)
-
+    def __init__(self, title, slug, content=None, meta=None):
+        self.title = title
+        self.slug = slug
         self.content = content
-        self.date = date or datetime.now()
-        self.top = top
-        self.publish = publish
-        self.layout = layout
-        self._tags = set()
-        self._category = None
 
-        if tags:
-            self._tags.update(tags)
+        meta = meta or {}
+        self.date = meta.pop('date', None) or datetime.now()
+        self.publish = meta.pop('publish', True)
+        self.layout = meta.pop('layout', Post.layout)
+        self.top = meta.pop('top', False)
+        self.tags = meta.pop('tags', [])
 
-        if category:
-            self.category = category
+        self.meta = meta
 
-    def __lt__(self, value):
-        return self.date < value.date
-
-    def add_tag(self, tag):
-        """Add tag"""
-        self._tags.add(tag.title)
-
-    @classmethod
-    def all(cls, post_filter=None):
-        """Get all posts
-        @param post_filter: a callable filter
-        @return: post list
-        """
-        posts = list(filter(post_filter, super(Post, cls).all()))
-        return sorted(posts, reverse=True)
-
-    @classmethod
-    def top_posts(cls):
-        """Get all top posts"""
-        return cls.all(lambda p: p.top)
+    def __getattr__(self, key):
+        try:
+            return super(Post, self).__getattr__(key)
+        except:
+            pass
+        return self.meta.get(key)
 
     @property
-    def tags(self):
-        """Get all tags"""
-        return [Tag.get(tag) for tag in self._tags]
+    def url(self):
+        relative_url = path_to_url(self.file_path)
+        if not relative_url.startswith('/'):
+            relative_url = '/'+relative_url
+        return relative_url
 
     @property
-    def catetory(self):
-        return Category.get(self._category)
-
-    @catetory.setter
-    def category(self, value):
-        if value:
-            self._category = value.title
-        else:
-            self._category = None
-
-
-class Tag(BaseModel):
-    """Tag"""
-
-    layout = 'tag'
-
-    def __init__(self, title):
-        super(Tag, self).__init__(title=title, slug=title)
-
-    @property
-    def posts(self):
-        """Get all posts that have this tag"""
-        return list(filter(lambda p: self.title in p._tags, Post.all()))
-
-
-class Category(Tag):
-    """Category"""
-
-    layout = 'category'
-
-    @property
-    def posts(self):
-        """Get all posts that belongs to this category"""
-        return filter(lambda p: p._category==self.title, Post.all())
+    def file_path(self):
+        template = configs.path.post
+        return url_safe(template.format(**self.__dict__))
--- test/test_model.py
+++ test/test_model.py
@@ -7,40 +7,36 @@
 from __future__ import unicode_literals
 
 import unittest
-from peanut.model import Post, Tag, Category
+from datetime import datetime
+from peanut.model import Post
+from peanut.options import configs
 
 
 class TestModel(unittest.TestCase):
-    def test_base_model(self):
-        tag = Tag('test')
-        Tag.path_template = '/tags/{slug}.html'
-        self.assertEqual(tag.title, 'test')
-        self.assertEqual(tag.slug, 'test')
-        self.assertListEqual(list(tag.posts), [])
-        self.assertEqual(tag.url, '/tags/test.html')
-        self.assertEqual(tag.file_path, '/tags/test.html')
-
-    def test_relationship(self):
-        post = Post.create('Hello world', 'hello_world')
-        tag = Tag.create('test')
-        category = Category.create('test')
-
-        post.add_tag(tag)
-        post.category = category
-
-        self.assertIn(tag, post.tags)
-        self.assertIn(post, list(tag.posts))
-        self.assertIn(post, list(category.posts))
-        self.assertIs(post.category, category)
-
-        category_new = Category.create('test_again')
-        post.category = category_new
-
-        self.assertIs(post.category, category_new)
-        self.assertListEqual(list(category.posts), [])
-
-
     def test_post(self):
-        post_a = Post.create('Post a', 'post_a', top=False, publish=True)
-        post_b = Post.create('Post b', 'post_b', top=True, publish=False)
-        self.assertListEqual(list(Post.all(lambda p: p.top)), [post_b])
+        meta = {
+            'date': None,
+            'tags': ['hello', 'world'],
+            'layout': None,
+            'top': True,
+            'publish': False,
+            'author': 'zqqf16',
+        }
+        post = Post('Hello world', 'hello_world', 'Hello world', meta)
+
+        now = datetime.now()
+
+        self.assertEqual(post.title, 'Hello world')
+        self.assertEqual(post.slug, 'hello_world')
+        self.assertEqual(post.author, 'zqqf16')
+        self.assertEqual(post.date.year, now.year)
+        self.assertTrue(post.top)
+        self.assertFalse(post.publish)
+
+        configs.path.post = 'posts/{slug}.html'
+        self.assertEqual(post.file_path, 'posts/hello_world.html')
+        self.assertEqual(post.url, '/posts/hello_world.html')
+
+        configs.path.post = 'posts/{title}.html'
+        self.assertEqual(post.file_path, 'posts/Hello-world.html')
+        self.assertEqual(post.url, '/posts/Hello-world.html')
